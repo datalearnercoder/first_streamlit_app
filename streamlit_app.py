42520833459f5bb120ba4934cc302a0d51a630e1
datalearnercoder <114033746+datalearnercoder@users.noreply.github.com>
2022-09-21 21:52:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 
 import streamlit
 
-streamlit.title('hello')
-streamlit.header('breakfast menu')
-streamlit.text('avocado toast')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
